e61bdc0339fb8c13f42ee9218258a48c9be49b7c
Peter John Smith <peter@visibility.net>
2017-04-13 10:10:22 +0100

    Added demo of two lists

---
+++ examples/HttpHelper.service.ts
@@ -0,0 +1,10 @@
+// interface IHttpHelper{
+
+// }
+// angular.module('app').factory('HttpHelper', function(){
+// let fac: IHttpHelper = {
+
+// };
+
+// return fac;
+// });
--- examples/ps-alert.component.ts
+++ examples/ps-alert.component.ts
@@ -19,40 +19,37 @@ angular.module("app").component('psAlert', {
     <a class="tiny button" href="" ng-click="vm.toggle()">
         ps-alert
     </a>
-    <div pageslide 
-            ps-side="bottom" 
-            ps-open="vm.checked"
-            ps-key-listener="true"
-            ps-body-class="true"
-            ps-auto-close="true>
+    <div pageslide ps-side="bottom" 
+        ps-open="vm.checked" 
+        ps-key-listener="true" 
+        ps-body-class="true" 
+        ps-auto-close="true">
         <div>
-                <h2>ps-alert</h2>           
-                <div ng-if="vm.checked">
-                  <h4>ps-alert</h4>
-                  <ol>
+            <h2>ps-alert</h2>
+            <div ng-if=" vm.checked ">
+                <h4>ps-alert</h4>
+                <ol>
                     <li>TODO: fix height to contents or allow scroll</li>
                     <li>invoke from current view as error handler</li>
                     <li>invoke from $window.onerror in nav.component</li>
                     <li>Add a $timeout to close the pageslide after a few seconds</li>
                     <li>Add a close icon</li>
-                  </ol>
-                  <h4>params</h4>
-                  <ol>
+                </ol>
+                <h4>params</h4>
+                <ol>
                     <li>title</li>
                     <li>message</li>
                     <li>actions</li>
-                  </ol>                  
-                  <h4>actions</h4>
-                  <ol>
+                </ol>
+                <h4>actions</h4>
+                <ol>
                     <li>fuged it</li>
                     <li>report it</li>
-                  </ol>
-                  <div>[ nav, esc, click ] to dismiss</div>   
-                </div>                             
+                </ol>
+                <div>[ nav, esc, click ] to dismiss</div>
             </div>
         </div>
+    </div>
+</div>
 `
 });
-/*
-
- */
